be324e6e0bca46dc5943479d863e6f7ed304f536
damien <damien@damienui-MacBookAir.local>
2023-06-12 18:48:47 +0900

    modified

--- model/CapGenerator.py
+++ model/CapGenerator.py
@@ -106,6 +106,7 @@ class CLIPTextGenerator:
             print('using label prompts...')
             str_label = " ".join(label.lower().split("_"))
             self.context_options = ['Video of {} shows'.format(str_label)]
+            print(self.context_options)
             prompt_len = len(self.context_options[0].split(" "))
             # self.context_options = [""]
             # prompt_len = 0

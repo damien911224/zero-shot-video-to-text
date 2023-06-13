c90793d945982dda5a6cded7a28a0fc63f8f71c4
damien <damien@damienui-MacBookAir.local>
2023-06-13 17:27:50 +0900

    modified

--- make_data.py
+++ make_data.py
@@ -22,7 +22,7 @@ def get_parser():
     parser.add_argument("--lm_model", type=str, default="gpt-2", help="gpt-2 or gpt-neo")
     parser.add_argument("--db_filter_path", type=str, default=None, help="file to filter db items, e.g karpathy split")
     parser.add_argument("--clip_checkpoints", type=str, default="./clip_checkpoints", help="path to CLIP")
-    parser.add_argument("--target_seq_length", type=int, default=30)
+    parser.add_argument("--target_seq_length", type=int, default=64)
     parser.add_argument("--cond_text", type=str, default="Image of a")
     parser.add_argument("--token_wise", action="store_true", help="Should we step the optimization at each token gen")
     parser.add_argument("--num_dummy_tokens", type=int, default=5)
